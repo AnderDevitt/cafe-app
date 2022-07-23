a3b56032e7da54d09696ab937efb435c3c3f6d57
andrew <gcab022116@coderacademy.edu.au>
2022-07-23 22:13:49 +1000

    changed header and footer to components and moved the style from css to styling.js

--- src/components/Employees.js
+++ src/components/Employees.js
@@ -1,16 +1,17 @@
 import Employee from "./Employee"
 import { useGlobalState } from "../utils/stateContext"
+import { ActiveStaff } from "./style/styling" 
 
 const Employees = () => {
     const {store} = useGlobalState()
     const {employeeList} = store
     
     return (
-        <>
+        <ActiveStaff>
             {employeeList.map(employee =>
                 <Employee key={employee.id} employee={employee} />    
             )}
-        </>
+        </ActiveStaff>
     )
 }
 
