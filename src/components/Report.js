956c7191630ec2a2d2f2d936725a46e5bb800b3b
RaDevy <ranjithrajesh256@gmail.com>
2022-08-03 18:58:30 +1000

    add empty state

--- src/components/Report.js
+++ src/components/Report.js
@@ -14,13 +14,17 @@ function Report() {
   const { shiftList } = store;
   const moment = require('moment');
   const [value, onChange] = useState(new Date());
-  let shifts = shiftList.filter((shift) => moment(value).format('YYYY-MM-DD') === shift.date) || "no shifts"
+  let shifts = shiftList.filter((shift) => moment(value).format('YYYY-MM-DD') === shift.date)
+  console.log(shifts.length == 0)
   return (
     <div className="report-list">
       <Calendar onChange={onChange} value={value} />
       <CSVLink data={shifts}>
         Download me
       </CSVLink>
+      {shifts.length == 0 && (
+        <h3> No shifts on this date </h3>
+      )}
       {shiftList.map((shift) => (
         <ReportItem
           key={shift.id}
