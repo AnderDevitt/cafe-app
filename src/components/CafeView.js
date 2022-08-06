c26f57e3d4769b0686301289021c3b303933d081
andrew <gcab022116@coderacademy.edu.au>
2022-08-06 14:10:46 +1000

    got rid of the dpeendancy warning for dispatch by moving it into the dependancy array with location

--- src/components/CafeView.js
+++ src/components/CafeView.js
@@ -2,19 +2,18 @@ import Employee from "./Employee"
 // import Shift from "./Shift" 
 import { useGlobalState } from "../utils/stateContext"
 // import { ActiveStaff, CurrentShift } from "./style/styling" 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
+// import React, { useState } from 'react';
 import { Typography } from "@mui/material"
-// import Clock from 'react-clock';
-// import 'react-clock/dist/Clock.css'
+
 import { ActiveStaff, EmployeeContainer } from "./style/styling" 
 import { getEmployees } from '../services/employeeServices';
 import { useLocation } from 'react-router-dom'
 
-
 const CafeView = () => {
     const {store, dispatch} = useGlobalState()
     const {employeeList} = store
-    // const [value, setValue] = useState(new Date());
+    
 
     const location = useLocation()
     console.log(location)
@@ -32,46 +31,26 @@ const CafeView = () => {
           .catch(e => {console.log(e)})
           }  
         },
-        [location] // triggers the useEffect to refresh list of employees when location changes
+        [dispatch, location] // triggers the useEffect to refresh list of employees when location changes
       )
-
-    // useEffect(() => {
-    //     const interval = setInterval(() => setValue(new Date()), 1000);
-    //     return () => {
-    //       clearInterval(interval);
-    //     };
-    //   }, []);
-    
 
     return (
         <>
-            {/* <div>
-                <p>Current time:</p>
-                <Clock value={value} />
-            </div> */}
+            
             <EmployeeContainer>
                 <Typography variant="h4">Available Cafe Staff</Typography>
                 <ActiveStaff>
                     {employeeList.map(employee =>
                     <>
-                        <Employee key={employee.id} employee={employee} />  
-                        
+                        <Employee key={employee.id} employee={employee} />         
                     </>
                           
                     )}
                 </ActiveStaff>
             </EmployeeContainer>
-            {/* <div>
-                <Typography variant="h4">Current Shifts</Typography>
-                <CurrentShift>
-                    {shiftList.map(shift =>
-                        <Shift key={shift.id} shift={shift} />
-                    )}
-                </CurrentShift>
-            </div> */}
-            
         </>
     )
 }
 
 export default CafeView
+
